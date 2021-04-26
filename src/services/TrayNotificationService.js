0e664322d6291ffcddd1361fb918fe8a74cd7821
LukasVyslocky <lukas.vyslocky@gmail.com>
2021-04-26 08:48:59 +0100

    fixes, added notification on countdown

--- src/services/TrayNotificationService.js
+++ src/services/TrayNotificationService.js
@@ -1,5 +1,6 @@
 const nativeImage = require('electron').nativeImage;
 import {ShowWindowService} from "./ShowWindowService";
+
 const path = require("path");
 
 let tray;
@@ -7,13 +8,25 @@ let timing = false;
 let notif = false
 let downloadTimer = undefined;
 
+function humanReadableTime(seconds) {
+    const d = Number(seconds);
+    const h = Math.floor(d / 3600);
+    const m = Math.floor(d % 3600 / 60);
+    const s = Math.floor(d % 3600 % 60);
+
+    const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay + sDisplay;
+
+}
+
 ShowWindowService.on('countdown', (time) => {
-    if (!downloadTimer && time < 10)
-    {
+    tray.setToolTip('Eye Guard' + (time ? ' ( ' +  humanReadableTime(time) + ' left to break ) ' : ''));
+    if (!downloadTimer && time < 10) {
         let timeleft = 10;
         downloadTimer = setInterval(function () {
-            if (timeleft <= 0)
-            {
+            if (timeleft <= 0) {
                 setNormal();
                 stopIntervalTimer();
             }
@@ -24,8 +37,7 @@ ShowWindowService.on('countdown', (time) => {
 })
 
 function stopIntervalTimer() {
-    if (downloadTimer)
-    {
+    if (downloadTimer) {
         clearInterval(downloadTimer);
         downloadTimer = undefined;
     }
@@ -43,8 +55,7 @@ ShowWindowService.on('finished', () => {
 });
 
 function init(t) {
-    if (tray)
-    {
+    if (tray) {
         start();
         return;
     }
@@ -52,8 +63,7 @@ function init(t) {
 }
 
 function setNotif() {
-    if (!tray)
-    {
+    if (!tray) {
         return;
     }
     tray.setImage(nativeImage.createFromPath(path.join(__dirname, '..', 'public', 'icons', 'chronometer-notif256x256.png')));
@@ -61,8 +71,7 @@ function setNotif() {
 }
 
 function setNormal() {
-    if (!tray)
-    {
+    if (!tray) {
         return;
     }
     stopIntervalTimer();
